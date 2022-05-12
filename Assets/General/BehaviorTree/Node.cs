34889db33c3e37e8d8b11df46a525b9e4b8bf5e0
MaximeVg <49032976+DevilSky93@users.noreply.github.com>
2022-05-12 15:09:57 +0200

    Add comment for behavior tree

--- Assets/General/BehaviorTree/Node.cs
+++ Assets/General/BehaviorTree/Node.cs
@@ -2,6 +2,9 @@ using System.Collections.Generic;
 
 namespace BehaviorTree
 {
+    /// <summary>
+    /// State of the node
+    /// </summary>
     public enum NodeState
     {
         RUNNING,
@@ -9,20 +12,43 @@ namespace BehaviorTree
         FAILURE
     }
 
+    /// <summary>
+    /// Node that is connected to the root, will be evaluated by the agent
+    /// </summary>
     public class Node
     {
+        /// <summary>
+        /// Current state of the node
+        /// </summary>
         protected NodeState State;
 
+        /// <summary>
+        /// Parent of the current node
+        /// </summary>
         public Node Parent { get; private set; }
-        protected List<Node> Children = new List<Node>();
+        /// <summary>
+        /// Children node that the node will have
+        /// </summary>
+        protected readonly List<Node> Children = new();
 
-        private readonly Dictionary<string, object> _dataContext = new Dictionary<string, object>();
+        /// <summary>
+        /// Dictionary that will contain information stocked by the agent
+        /// </summary>
+        private readonly Dictionary<string, object> _dataContext = new();
 
-        public Node()
+        /// <summary>
+        /// Create node with no parent
+        /// </summary>
+        protected Node()
         {
             Parent = null;
         }
 
+        /// <summary>
+        /// Attach child node with this current node
+        /// </summary>
+        /// <param name="node">Child node to be attached</param>
+        /// <returns>Return current node for chaining</returns>
         public Node Attach(Node node)
         {
             node.Parent = this;
@@ -30,13 +56,27 @@ namespace BehaviorTree
             return this;
         }
 
+        /// <summary>
+        /// Evaluate children node
+        /// </summary>
+        /// <returns>Return FAILURE by default</returns>
         public virtual NodeState Evaluate() => NodeState.FAILURE;
 
+        /// <summary>
+        /// Add data to the dictionary
+        /// </summary>
+        /// <param name="key">Key to be add</param>
+        /// <param name="value">Value to be add</param>
         public void SetData(string key, object value)
         {
             _dataContext[key] = value;
         }
 
+        /// <summary>
+        /// Add data to the dictionary of the root node
+        /// </summary>
+        /// <param name="key">Key to be add</param>
+        /// <param name="value">Value to be add</param>
         public void SetDataToRoot(string key, object value)
         {
             Node node = Parent;
@@ -48,6 +88,11 @@ namespace BehaviorTree
             node._dataContext[key] = value;
         }
 
+        /// <summary>
+        /// Retrieve value from dictionary
+        /// </summary>
+        /// <param name="key">Key used to look for data</param>
+        /// <returns>Return value of the dictionary according to the key</returns>
         protected object GetData(string key)
         {
             if (_dataContext.TryGetValue(key, out var value))
@@ -58,9 +103,14 @@ namespace BehaviorTree
             return Parent?.GetData(key);
         }
 
+        /// <summary>
+        /// Remove data from the dictionary
+        /// </summary>
+        /// <param name="key">Key remove from the dictionary</param>
+        /// <returns>Return true if the removal succeeded, false if not</returns>
         protected bool RemoveData(string key)
         {
-            if (_dataContext.TryGetValue(key, out var value))
+            if (_dataContext.TryGetValue(key, out _))
             {
                 _dataContext.Remove(key);
                 return true;
--- Assets/General/BehaviorTree/Sequence.cs
+++ Assets/General/BehaviorTree/Sequence.cs
@@ -2,13 +2,16 @@
 
 namespace BehaviorTree
 {
+    /// <summary>
+    /// Sequence nodes contain one or more children. Upon execution, it executes every child and fails when one of the children fails.
+    /// </summary>
     [VisualNode(displayAsBlock: true)]
     public class Sequence : Node
     {
-        public Sequence()
-        {
-        }
-
+        /// <summary>
+        /// Evaluate the node
+        /// </summary>
+        /// <returns>Return FAILURE if a child node failed, RUNNING if a child is running after every evaluated node, SUCCESS if none of the child is running or failing </returns>
         public override NodeState Evaluate()
         {
             var anyChildIsRunning = false;
