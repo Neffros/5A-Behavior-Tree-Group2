447065632a1eb1b50e07ba4fa5650a84f8926864
Lior <diler.lior@gmail.com>
2022-05-12 20:38:18 +0200

    added buildings and prevent agent spawns in buildings

--- Assets/Crowd Control/Scripts/GameManager.cs
+++ Assets/Crowd Control/Scripts/GameManager.cs
@@ -10,6 +10,8 @@ public class GameManager : MonoBehaviour
     private List<GameObject> goals;
 
     public GameObject agentPrefab;
+    public GameObject buildingPrefab;
+    
     public int nbAgents;
     public Vector3 minSpawnPoint;
     public Vector3 maxSpawnPoint;
@@ -34,14 +36,23 @@ public class GameManager : MonoBehaviour
     #endregion
 
 
+
     private void Start()
     {
         for (int i = 0; i < nbAgents; ++i)
         {
-            Instantiate(agentPrefab,
-                new Vector3(Random.Range(minSpawnPoint.x, maxSpawnPoint.x),
-                    Random.Range(minSpawnPoint.y, maxSpawnPoint.y), Random.Range(minSpawnPoint.z, maxSpawnPoint.z)),
-                Quaternion.identity);
+            Vector3 spawnPoint = new Vector3(Random.Range(minSpawnPoint.x, maxSpawnPoint.x),
+                Random.Range(minSpawnPoint.y, maxSpawnPoint.y), Random.Range(minSpawnPoint.z, maxSpawnPoint.z));
+            
+            RaycastHit hit;
+            if (Physics.Raycast(spawnPoint + Vector3.up*50, Vector3.down, out hit, 100, LayerMask.GetMask("Building")))
+            {
+                --i;
+                continue;
+                
+            }
+
+            Instantiate(agentPrefab, spawnPoint, Quaternion.identity);
         }
     }
 
