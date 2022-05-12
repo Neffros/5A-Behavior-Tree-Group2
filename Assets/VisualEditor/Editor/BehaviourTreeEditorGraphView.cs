be2af698624d29f66a3b8aaf2d5044595d78b830
sikny <pamartqu@gmail.com>
2022-05-12 16:39:37 +0200

    layout for selector and sequence nodes + auto get nodes types for creation context menu

--- Assets/VisualEditor/Editor/BehaviourTreeEditorGraphView.cs
+++ Assets/VisualEditor/Editor/BehaviourTreeEditorGraphView.cs
@@ -22,7 +22,7 @@ namespace VisualEditor.Editor {
                 name = "GridBackground"
             };
             Insert(0, gridBackground);
-            SetupZoom(ContentZoomer.DefaultMinScale, ContentZoomer.DefaultMaxScale);
+            SetupZoom(ContentZoomer.DefaultMinScale, 2.0f);
             
             viewTransform.scale = Vector3.one * 0.25f;
             
@@ -41,18 +41,41 @@ namespace VisualEditor.Editor {
         }
 
         public override void BuildContextualMenu(ContextualMenuPopulateEvent evt) {
-            evt.menu.AppendAction("Create Node/Selector", action => {
-                
-            });
-            evt.menu.AppendAction("Create Node/Sequence", action => {
-                
-            });
             foreach (var nodeMetadata in _nodeReflectionEngine.Metadata) {
-                evt.menu.AppendAction("Create Node/Custom Node/" + nodeMetadata.Value.Name, action => {
+                evt.menu.AppendAction("Create Node/" + nodeMetadata.Value.Name, action => {
                     var node = new VisualElement();
                     node.AddToClassList("node");
                     node.styleSheets.Add(_nodeStyleSheet);
-                    node.Add(new TextElement {text = nodeMetadata.Value.Name});
+                    if (nodeMetadata.Value.DisplayAsBlock) {    // sequence
+                        node.AddToClassList("node-sequence");
+
+                        var vContainer = new VisualElement();
+                        vContainer.AddToClassList("v-container");
+                        vContainer.Add(new TextElement { text = nodeMetadata.Value.Name });
+
+                        var hContainer = new VisualElement();
+                        hContainer.AddToClassList("h-container");
+                        
+                        // add all nodes here
+                        
+                        vContainer.Add(hContainer);
+                        node.Add(vContainer);
+                        
+                        node.Add(new Button(() => {
+                        
+                        }) {
+                            text = "+",
+                        });
+                    }
+                    else {
+                        node.Add(new TextElement { text = nodeMetadata.Value.Name });
+                        var buttonAdd = new Button(() => {
+                        
+                        }) {
+                            text = "+",
+                        };
+                        node.Add(buttonAdd);
+                    }
                     contentViewContainer.Add(node);
                     _nodes.Add(node);
                 });
