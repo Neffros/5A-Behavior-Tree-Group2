0bfccc451c3ba425bee6c587edf0c9ef75cf51e9
Cyphall <cyphallbraus@gmail.com>
2022-05-12 16:04:06 +0200

    [FIGHT] added boss animations

--- Assets/Fight/Scripts/CameraRotator.cs
+++ Assets/Fight/Scripts/CameraRotator.cs
@@ -7,7 +7,7 @@ namespace Fight
 		[SerializeField]
 		[Range(0.0f, 10.0f)]
 		private float _mouseSensitivity = 3;
-		
+
 		[SerializeField]
 		private Transform _playerTransform;
 
@@ -24,18 +24,19 @@ namespace Fight
 				x = Input.GetAxis("Mouse X"),
 				y = Input.GetAxis("Mouse Y")
 			};
-		
+
 			delta *= _mouseSensitivity;
 
 			Vector3 yRotation = _playerTransform.localEulerAngles;
 			yRotation.y += delta.x;
 			_playerTransform.localEulerAngles = yRotation;
-			
+
 			Vector3 xRotation = transform.localEulerAngles;
 			if (xRotation.x > 180)
 			{
 				xRotation.x -= 360;
 			}
+
 			xRotation.x = Mathf.Clamp(xRotation.x - delta.y, -89, 89);
 			transform.localEulerAngles = xRotation;
 		}
