fdb5e14bd55667a40e360431bda762ef882abc55
Sami DALIL <MN6195@equans.com>
2022-05-12 14:38:23 +0200

    Added BehaviorTree scriptable object and NodeEditorInstanceMetadata

---
+++ Assets/General/BehaviorTreeSerializer/Data/BehaviorTreeObject.cs
@@ -0,0 +1,100 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BehaviorTreeSerializer.Data
+{
+    /// <summary>
+    /// Represents the tree of nodes in the visual editor
+    /// </summary>
+    [CreateAssetMenu(fileName = "BehaviorTree", menuName = "Behavior Tree/Create Object", order = 1)]
+    public class BehaviorTreeObject : ScriptableObject
+    {
+        #region Unity Fields
+
+        [SerializeField, Tooltip("Maps a node to its ID")]
+        private Dictionary<Guid, NodeEditorInstanceMetadata> _idToNode;
+
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        /// Gets or sets the dictionary of nodes by their IDs
+        /// </summary>
+        public Dictionary<Guid, NodeEditorInstanceMetadata> IdToNode { get; set; }
+
+        #endregion
+
+        #region Public Methods
+
+        /// <summary>
+        /// Adds child ID to parent node's children IDs list
+        /// </summary>
+        /// <param name="parentId">ID of the parent</param>
+        /// <param name="childId">ID of the child</param>
+        public void AddChild(Guid parentId, Guid childId)
+        {
+            if (!this.IdToNode.ContainsKey(parentId))
+                throw new Exception("Parent not found");
+            if (!this.IdToNode.ContainsKey(childId))
+                throw new Exception("Child not found");
+
+            this.IdToNode[parentId].ChildrenIds.Add(childId);
+        }
+
+        /// <summary>
+        /// Adds a node to the store and adds it as child of its parent if given
+        /// </summary>
+        /// <param name="nodeTypeInternalName">The node type to instantiate</param>
+        /// <param name="properties">Properties of the node type with default values</param>
+        /// <param name="positionInEditor">Position on the grid</param>
+        /// <param name="parentId">ID of the parent node</param>
+        /// <returns>The instantiated node metadata</returns>
+        public NodeEditorInstanceMetadata AddNode(string nodeTypeInternalName, Dictionary<string, object> properties, Vector2 positionInEditor, Guid? parentId)
+        {
+            var node = new NodeEditorInstanceMetadata(nodeTypeInternalName, properties, positionInEditor, parentId);
+
+            this.IdToNode.Add(node.Id, node);
+
+            if (parentId.HasValue)
+                this.AddChild(parentId.Value, node.Id);
+
+            return node;
+        }
+
+        /// <summary>
+        /// Sets the property of a node
+        /// </summary>
+        /// <param name="nodeId">ID of the node</param>
+        /// <param name="property">Property name</param>
+        /// <param name="value">Value of the property</param>
+        public void SetNodeProperty(Guid nodeId, string property, object value)
+        {
+            if (!this.IdToNode.ContainsKey(nodeId))
+                throw new Exception("Node not found");
+            if (!this.IdToNode[nodeId].Properties.ContainsKey(property))
+                throw new Exception("Property not found");
+
+            this.IdToNode[nodeId].Properties[property] = value;
+        }
+
+        #endregion
+
+        #region Public Static Methods
+
+        /// <summary>
+        /// Creates a BehaviorTreeObject with default properties
+        /// </summary>
+        /// <returns>A BehaviorTreeObject</returns>
+        public static BehaviorTreeObject Create()
+        {
+            var tree = ScriptableObject.CreateInstance<BehaviorTreeObject>();
+
+            tree.IdToNode = new Dictionary<Guid, NodeEditorInstanceMetadata>();
+            return tree;
+        }
+
+        #endregion
+    }
+}
---
+++ Assets/General/BehaviorTreeSerializer/Data/NodeEditorInstanceMetadata.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BehaviorTreeSerializer.Data
+{
+    /// <summary>
+    /// Contains the metadata of a node in the visual editor
+    /// </summary>
+    [Serializable]
+    public class NodeEditorInstanceMetadata
+    {
+        #region Properties
+
+        /// <summary>
+        /// Gets or sets the list of chidren's IDs
+        /// </summary>
+        public List<Guid> ChildrenIds { get; set; }
+
+        /// <summary>
+        /// Gets or sets the node's ID
+        /// </summary>
+        public Guid Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets the node type's internal name
+        /// </summary>
+        public string NodeTypeInternalName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the parent node's ID
+        /// </summary>
+        public Guid ParentId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the position in the visual editor
+        /// </summary>
+        public Vector2 PositionInEditor { get; set; }
+
+        /// <summary>
+        /// Gets or sets the dictionary of properties of the node
+        /// </summary>
+        public Dictionary<string, object> Properties { get; set; }
+
+        #endregion
+
+        #region Constructor
+
+        /// <summary>
+        /// Class constructor
+        /// </summary>
+        public NodeEditorInstanceMetadata()
+        {
+        }
+
+        /// <summary>
+        /// Class constructor
+        /// </summary>
+        public NodeEditorInstanceMetadata(string nodeTypeInternalName, Dictionary<string, object> properties, Vector2 positionInEditor, Guid? parentId)
+        {
+            this.ChildrenIds = new List<Guid>();
+            this.Id = Guid.NewGuid();
+            this.NodeTypeInternalName = nodeTypeInternalName;
+            this.ParentId = parentId ?? Guid.Empty;
+            this.PositionInEditor = positionInEditor;
+            this.Properties = properties;
+        }
+
+        #endregion
+    }
+}
