a574c4a99a195c17624dcd3b2542c44a8001c6fb
Sami DALIL <MN6195@equans.com>
2022-05-13 02:57:08 +0200

    Initialization pattern for scriptable object Fix parent ID issue on remove methods

--- Assets/General/BehaviorTreeSerializer/Data/BehaviorTreeObject.cs
+++ Assets/General/BehaviorTreeSerializer/Data/BehaviorTreeObject.cs
@@ -16,6 +16,9 @@ namespace BehaviorTreeSerializer.Data
         [SerializeField, Tooltip("Maps a node to its ID")]
         private Dictionary<Guid, NodeEditorInstanceMetadata> _idToNode;
 
+        [SerializeField, HideInInspector]
+        private bool _initialized;
+
         [SerializeField, Tooltip("ID of the root node")]
         private Guid _rootId;
 
@@ -32,6 +35,18 @@ namespace BehaviorTreeSerializer.Data
         /// Gets or sets the ID of the root node
         /// </summary>
         public Guid RootId => this._rootId;
+
+        #endregion
+
+        #region Unity Callbacks
+
+        /// <summary>
+        /// Fired on serialized field edition in inspector
+        /// </summary>
+        private void OnValidate()
+        {
+            this.Initialize();
+        }
 
         #endregion
 
@@ -73,6 +88,32 @@ namespace BehaviorTreeSerializer.Data
         }
 
         /// <summary>
+        /// Initializes this BehaviorTreeObject with default properties
+        /// </summary>
+        /// <returns>This instance</returns>
+        public BehaviorTreeObject Initialize()
+        {
+            if (this._initialized)
+                return this;
+
+            var selectorName = typeof(Selector).Name;
+            var root = new NodeEditorInstanceMetadata(
+                selectorName,
+                NodeReflection.Engine.GetProperties(selectorName),
+                Vector2.zero,
+                null
+            );
+
+            this.IdToNode = new Dictionary<Guid, NodeEditorInstanceMetadata>();
+            this.IdToNode.Add(root.Id, root);
+            this._rootId = root.Id;
+
+            this._initialized = true;
+
+            return this;
+        }
+
+        /// <summary>
         /// Removes the child node from the parent
         /// </summary>
         /// <param name="parentId">ID of the parent node</param>
@@ -81,10 +122,12 @@ namespace BehaviorTreeSerializer.Data
         {
             if (!this.IdToNode.ContainsKey(parentId))
                 throw new Exception("Parent not found");
+
             if (!this.IdToNode[parentId].ChildrenIds.Contains(childId))
                 throw new Exception("Child not found");
 
             this.IdToNode[parentId].ChildrenIds.Remove(childId);
+            this.IdToNode[childId].ParentId = Guid.Empty;
         }
 
         /// <summary>
@@ -98,6 +141,9 @@ namespace BehaviorTreeSerializer.Data
 
             if (!this.IdToNode[nodeId].ParentId.Equals(Guid.Empty))
                 this.RemoveChild(this.IdToNode[nodeId].ParentId, nodeId);
+
+            foreach (var childId in this.IdToNode[nodeId].ChildrenIds)
+                this.IdToNode[childId].ParentId = Guid.Empty;
 
             this.IdToNode.Remove(nodeId);
         }
@@ -123,26 +169,10 @@ namespace BehaviorTreeSerializer.Data
         #region Public Static Methods
 
         /// <summary>
-        /// Creates a BehaviorTreeObject with default properties
+        /// Creates an initialized BehaviorTreeObject
         /// </summary>
         /// <returns>A BehaviorTreeObject</returns>
-        public static BehaviorTreeObject Create()
-        {
-            var tree = ScriptableObject.CreateInstance<BehaviorTreeObject>();
-            var selectorName = typeof(Selector).Name;
-            var root = new NodeEditorInstanceMetadata(
-                selectorName,
-                NodeReflection.Engine.GetProperties(selectorName),
-                Vector2.zero,
-                null
-            );
-
-            tree.IdToNode = new Dictionary<Guid, NodeEditorInstanceMetadata>();
-            tree.IdToNode.Add(root.Id, root);
-            tree._rootId = root.Id;
-
-            return tree;
-        }
+        public static BehaviorTreeObject Create() => ScriptableObject.CreateInstance<BehaviorTreeObject>().Initialize();
 
         #endregion
     }
